c133b6ad331a6059a93bad02f268e11bb8c3c5ed
John T <john.david.tringham@gmail.com>
2023-03-07 23:24:37 +0000

    Adding TODOs

--- removing_duplicates.py
+++ removing_duplicates.py
@@ -2,8 +2,11 @@
 import pandas as pd
 
 def worker():
-    list_of_cinemas = '2023-03-06_list_of_cinemas'
-    #london_cinemas_csv = pd
+    list_of_cinemas = '2023-03-0&_list_of_cinemas'
+    # london_cinemas_csv = pd.read_csv(list_of_cinemas)
+
+    # TODO - add de-duplication
+    # TODO - group all of London into one? Or categorize by postcode
 
 
 

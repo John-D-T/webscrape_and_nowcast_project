7d01c87f9fcc9d43da980de78d1ab48609685e09
John T <john.david.tringham@gmail.com>
2023-04-23 15:09:18 +0100

    adding python module to eventually add data checks for seasonality

---
+++ regression_analysis/seasonality_checker.py
@@ -0,0 +1,7 @@
+"""
+Plot all data sources - see if there is seasonality (regular fluctuations)
+   Seasonally adjust if needed
+
+interactive dummy?
+Also can eyeball residuals, to see first if it's worth adding.
+"""

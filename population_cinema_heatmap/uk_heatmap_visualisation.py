48e57af31100c21bafd04a6d0fe1e65ee840c366
John T <john.david.tringham@gmail.com>
2023-05-08 22:45:20 +0100

    working on heatmap

--- population_cinema_heatmap/uk_heatmap_visualisation.py
+++ population_cinema_heatmap/uk_heatmap_visualisation.py
@@ -4,6 +4,8 @@ PYTHON 3.8 (64 BIT)
 pip install GDAL-3.3.3-cp38-cp38-win_amd64.whl (https://www.lfd.uci.edu/~gohlke/pythonlibs/#gdal)
 pip install rasterio
 pip install pgeocode
+pip install folium (WIP)
+pip install geopy
 
 Population heatmap:
 https://towardsdatascience.com/creating-beautiful-population-density-maps-with-python-fcdd84035e06
@@ -26,9 +28,10 @@ from matplotlib import cm
 from matplotlib.colors import BoundaryNorm, LinearSegmentedColormap, ListedColormap
 import matplotlib.colors as colors
 
-import pgeocode
-import plotly.express as px
 import pandas as pd
+from geopy.geocoders import Nominatim
+import folium
+from folium.plugins import FastMarkerCluster
 
 
 def prepare_map_colour_scheme():
@@ -75,14 +78,39 @@ def generate_postcode_mapping():
     list_of_cinemas_df = pd.read_csv(os.path.join(os.path.dirname(os.getcwd()), 'google_maps_scraper', 'output', cinema_file), header=0)
     list_of_postcodes = list_of_cinemas_df['postcode'].tolist()
 
-    # TODO - passing postcodes into map
+    # TODO - convert postcodes into latitude-longitude
+    # TODO - plot latitude-longitude on map
+
+    # SOLN 1
+    # nomi = pgeocode.Nominatim('GB')
     # Issue with urllib.error.URLError: <urlopen error [SSL: CERTIFICATE_VERIFY_FAILED]
     # https://stackoverflow.com/questions/59521203/using-pgeocode-lib-of-python-to-find-the-latitude-and-longitude - WIP
-    nomi = pgeocode.Nominatim('GB')
 
+    # SOLN 2: https://towardsdatascience.com/geocode-with-python-161ec1e62b89
 
+    # SOLN 3: https://towardsdatascience.com/geopandas-101-plot-any-data-with-a-latitude-and-longitude-on-a-map-98e01944b972
+    from geopy.extra.rate_limiter import RateLimiter
 
+    locator = Nominatim(user_agent="myGeocoder")
+    geocode = RateLimiter(locator.geocode, min_delay_seconds=1)
+    list_of_cinemas_df['location'] = list_of_cinemas_df['postcode'].apply(geocode)
+    list_of_cinemas_df['point'] = list_of_cinemas_df['location'].apply(lambda loc: tuple(loc.point) if loc else None)
+    # split point column into latitude, longitude and altitude columns
+    list_of_cinemas_df[['latitude', 'longitude', 'altitude']] = pd.DataFrame(list_of_cinemas_df['point'].tolist(), index=list_of_cinemas_df.index)
 
+    # check for nulls - only 6 at the moment
+    list_of_cinemas_df.latitude.isnull().sum()
+
+    # removing nulls
+    list_of_cinemas_df = list_of_cinemas_df[pd.notnull(list_of_cinemas_df["latitude"])]
+
+    # https://realpython.com/python-folium-web-maps-from-data/#:~:text=Python's%20Folium%20library%20gives%20you,can%20share%20as%20a%20website.
+    # TODO - edit this to fit 'location' to the UK
+    map1 = folium.Map(
+        location=[59.338315, 18.089960],
+        tiles='cartodbpositron',
+        zoom_start=12,
+    )
 
 if __name__ == '__main__':
     #generate_heatmap()

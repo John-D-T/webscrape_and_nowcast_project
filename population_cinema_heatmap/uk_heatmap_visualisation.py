4f9ccdf3185c262df4187e3c11f00335878f7c03
John T <john.david.tringham@gmail.com>
2023-05-24 22:10:17 +0100

    heatmap works

--- population_cinema_heatmap/uk_heatmap_visualisation.py
+++ population_cinema_heatmap/uk_heatmap_visualisation.py
@@ -22,20 +22,14 @@ import rasterio
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
 from matplotlib import cm
-from matplotlib.colors import BoundaryNorm, LinearSegmentedColormap, ListedColormap
+from matplotlib.colors import ListedColormap
 import matplotlib.colors as colors
 
 import pandas as pd
 from geopy.geocoders import Nominatim
 import folium
-from folium.plugins import FastMarkerCluster
-
-import geopandas as gpd
-from shapely.geometry import Point, Polygon
-
 
 def prepare_map_colour_scheme():
     # for get_cmap, see this for colour schemes- https://matplotlib.org/stable/tutorials/colors/colormaps.html
@@ -55,9 +49,8 @@ def prepare_map_colour_scheme():
 
 def generate_heatmap():
     '''
-    Use csv containing 800~ cinemas (scraped from Google), and plot them (using postcodes) on a map of the UK
-    Then compare to a population of the UK (heatmap)
-    Comment on whether coverage is good - can add to intro (justification for study)
+    Plot population of the UK (heatmap)
+    Note that the max value is 1193 people per pixel (100 metres)
     '''
     norm, newcmap = prepare_map_colour_scheme()
 
@@ -66,15 +59,19 @@ def generate_heatmap():
     uk_worldpop_raster_tot = tif_file.read(1)
 
     uk_worldpop_raster_tot[uk_worldpop_raster_tot < 0] = None
+
+    heatmap_df = pd.DataFrame(uk_worldpop_raster_tot, columns=[''])
+
     plt.rcParams['figure.figsize'] = 10, 10
     plt.imshow(np.log10(uk_worldpop_raster_tot + 1), norm=norm, cmap=newcmap)
     bar = plt.colorbar(fraction=0.1)
 
-    # TODO - plot population density values on the right with each colour - note that the max value is 1193 people per pixel (100 metres)
-
 
 def generate_postcode_mapping():
-
+    """
+        Use csv containing 800~ cinemas (scraped from Google), and plot them (using postcodes) on a map of the UK
+    :return:
+    """
     # load data containing all cinemas in the UK (google scrape)
     cinema_file = "2023-04-27_cinema_and_post_codes.csv"
     list_of_cinemas_df = pd.read_csv(os.path.join(os.path.dirname(os.getcwd()), 'google_maps_scraper', 'output', cinema_file), header=0)
@@ -97,8 +94,6 @@ def generate_postcode_mapping():
     # removing nulls
     list_of_cinemas_df = list_of_cinemas_df[pd.notnull(list_of_cinemas_df["latitude"])]
 
-    list_of_cinemas_plt_df = list_of_cinemas_df
-
     # https://realpython.com/python-folium-web-maps-from-data/#:~:text=Python's%20Folium%20library%20gives%20you,can%20share%20as%20a%20website.
     map = folium.Map(
         location=[55.3781, 3.4360],
@@ -106,24 +101,10 @@ def generate_postcode_mapping():
         zoom_start=6
     )
     # https://snyk.io/advisor/python/folium/functions/folium.CircleMarker
-    list_of_cinemas_df.apply(lambda row: folium.CircleMarker(location=[row["latitude"], row["longitude"]], radius=2, fill=True).add_to(map), axis=1)
+    list_of_cinemas_df.apply(lambda row: folium.CircleMarker(location=[row["latitude"], row["longitude"]], radius=1, fill=True).add_to(map), axis=1)
     map.save("cinema_heatmap_zoom.html")
-    #map.save("cinema_heatmap_zoom.png")
-
-    # TODO - attempt a matplotlib plot - https://github.com/rylativity/identifying_wnv_locations-raw_repo-/blob/master/03-Geo_modeling.ipynb
-    geometry = [Point(xy) for xy in zip(list_of_cinemas_plt_df.Longitude, list_of_cinemas_plt_df.Latitude)]
-    list_of_cinemas_plt_df['geometry'] = geometry
-    list_of_cinemas_plt_df.drop(['Latitude', 'Longitude'], axis=1, inplace=True)
-    crs = {'init': 'epsg:4326'}
-    spray_locs = gpd.GeoDataFrame(list_of_cinemas_plt_df, crs=crs, geometry=geometry)
-    city_bound = gpd.read_file(os.path.join(os.getcwd(), "Great_Britain_shapefile", "gb_1km.shp"))
-
-    fig, ax = plt.subplots(figsize=(8, 8))
-    spray_locs.geometry.plot(marker='d', color='blue', markersize=1, ax=ax, label='Spray Loc')
-    list_of_cinemas_plt_df.plot(color='grey', ax=ax, alpha=0.4)
 
 
 if __name__ == '__main__':
-    #generate_heatmap()
-
+    generate_heatmap()
     generate_postcode_mapping()

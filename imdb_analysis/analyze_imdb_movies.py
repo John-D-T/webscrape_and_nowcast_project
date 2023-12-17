c3209b0a2e119553214c249731535e08faa5da4a
John T <john.david.tringham@gmail.com>
2023-12-17 15:33:34 +0000

    refactor: improving syntax for imdb_movies

--- imdb_analysis/analyze_imdb_movies.py
+++ imdb_analysis/analyze_imdb_movies.py
@@ -9,35 +9,36 @@ from scipy.stats import ks_2samp
 from imdb_analysis.plots import plot_average_imdb_ratings_per_year
 
 
-def concatenate_imdb_movies(extension='csv'):
+def concatenate_imdb_movies(extension='csv', file_name="all_movies.csv"):
     """
     One time function to combine all the individual imdb files into one.
     :return:
     """
-    path = os.path.join(os.getcwd(), 'genre')
-    os.chdir(path)
+    os.chdir(os.path.join(os.getcwd(), 'genre'))
+
     all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 
     # combine all files in the list
     combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
     # export to csv
-    combined_csv.to_csv("all_movies.csv", index=False, encoding='utf-8-sig')
+    combined_csv.to_csv(file_name, index=False, encoding='utf-8-sig')
 
 
-def plot_imdb_distributions(df):
+def plot_imdb_distributions(df, range_start=2004, range_end=2023, range_interval=1):
     """
     Function to plot ratings for all imdb movies over the years
 
     :param df: dataframe containing imdb ratings for movies.
+    :param range_start: start of year range (default is 2004)
+    :param range_end: end of year range (default is 2023)
+    :param range_interval: interval of year range (default is 1)
     :return:
     """
 
     # We split the ratings by year (to plot as seperate lines)
-    def generate_imdb_dictionary(df):
-        years = [year for year in range(2004, 2023, 1)]
-        return {y: df[df['year_adjusted'] == y] for y in years}
-
-    df_plot_dictionary = generate_imdb_dictionary(df)
+    df_plot_dictionary = generate_imdb_dictionary_split_by_year(df=df, range_start=range_start,
+                                                                range_end=range_end,
+                                                                range_interval=range_interval)
 
     # we now plot these 20 individual lines onto the same graph
     for year in df_plot_dictionary:
@@ -48,6 +49,26 @@ def plot_imdb_distributions(df):
     plt.show()
 
     return df_plot_dictionary
+
+
+def generate_imdb_dictionary_split_by_year(df, range_start, range_end, range_interval):
+    """
+    Function which processes a dataframe containing imdb ratings, splits these up by year,
+    then returns a dictionary with the key:value pairs being:
+        key: year
+        value: dataframe filtered on the 'year_adjusted' equalling that year
+
+    :param df: dataframe containing imdb ratings for movies.
+    :param range_start: start of year range (default is 2004)
+    :param range_end: end of year range (default is 2023)
+    :param range_interval: interval of year range (default is 1)
+
+    :return: A dictionary containing:
+        key: year
+        value: dataframe filtered on the 'year_adjusted' equalling that year
+    """
+    years = [year for year in range(range_start, range_end, range_interval)]
+    return {y: df[df['year_adjusted'] == y] for y in years}
 
 
 def two_sample_kolmogorov_smirnov_test(df, year_one, year_two):

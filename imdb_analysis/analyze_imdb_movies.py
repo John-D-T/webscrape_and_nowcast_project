7cd3a2a70c41a5fd5da8f5c10b0e0dce82e49c0d
JohnDT <59450372+John-D-T@users.noreply.github.com>
2023-12-17 17:23:32 +0000

    PR: cleaning up imdb_movies.py module (#6)
    * refactor: clean-up imdb code
    
    * refactor: improving syntax for imdb_movies and cleanup of useless files.
    
    * refactor: improving syntax for imdb_movies

--- imdb_analysis/analyze_imdb_movies.py
+++ imdb_analysis/analyze_imdb_movies.py
@@ -6,103 +6,93 @@ import pandas as pd
 import seaborn as sns
 from scipy.stats import ks_2samp
 
-from common.latex_file_generator import save_df_as_image
+from imdb_analysis.plots import plot_average_imdb_ratings_per_year
 
 
-def get_average_imdb_ratings_all_movies():
-    # 368,000 movies pre data cleaning
-    pd.set_option('display.max_rows', 500)
+def concatenate_imdb_movies(extension='csv', file_name="all_movies.csv"):
+    """
+    One time function to combine all the individual imdb files into one.
+    :return:
+    """
+    os.chdir(os.path.join(os.getcwd(), 'genre'))
 
-    imdb_df = pd.read_csv(os.path.join(os.getcwd(), 'genre', 'all_movies.csv'))
-    imdb_df = imdb_df[imdb_df['votes'].notna()]
-    imdb_df['votes'] = imdb_df['votes'].replace({'K': '000', 'M': '000000'}, regex=True).map(pd.eval).astype(int)
-    imdb_df['user_rating_adjusted'] = pd.to_numeric(imdb_df['votes'], errors='coerce').fillna(0)
-    # imdb_df = imdb_df[imdb_df['user_rating_adjusted'] > 5000]
-
-    imdb_df = imdb_df[imdb_df['gross(in $)'] > 1000000]
-
-    imdb_df['rating_adjusted'] = pd.to_numeric(imdb_df['rating'], errors='coerce').fillna(0)
-    pd.to_numeric(imdb_df['rating_adjusted'])
-
-    imdb_df['year_adjusted'] = pd.to_numeric(imdb_df['year'], errors='coerce').fillna(0)
-    pd.to_numeric(imdb_df['year_adjusted'])
-    imdb_df['year_adjusted'] = imdb_df['year_adjusted'].abs()
-    imdb_df = imdb_df[imdb_df['year_adjusted'] > 1999]
-
-    # https://stackoverflow.com/questions/44522741/pandas-mean-typeerror-could-not-convert-to-numeric
-    imdb_df_grouped = imdb_df.groupby('year_adjusted')['rating_adjusted'].mean().reset_index()
-
-    save_df_as_image(df=imdb_df_grouped, file_name='imdb_rating_368k')
-
-    return imdb_df
-
-
-def concatenate_imdb_movies():
-    extension = 'csv'
-    path = os.path.join(os.getcwd(), 'genre')
-    os.chdir(path)
     all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 
     # combine all files in the list
     combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
     # export to csv
-    combined_csv.to_csv("all_movies.csv", index=False, encoding='utf-8-sig')
+    combined_csv.to_csv(file_name, index=False, encoding='utf-8-sig')
 
 
-def plot_imdb_distributions(df):
+def plot_imdb_distributions(df, range_start=2004, range_end=2023, range_interval=1):
+    """
+    Function to plot ratings for all imdb movies over the years
+
+    :param df: dataframe containing imdb ratings for movies.
+    :param range_start: start of year range (default is 2004)
+    :param range_end: end of year range (default is 2023)
+    :param range_interval: interval of year range (default is 1)
+    :return:
+    """
 
     # We split the ratings by year (to plot as seperate lines)
-    imdb_2022 = df.loc[df['year_adjusted'] == 2022]
-    imdb_2021 = df.loc[df['year_adjusted'] == 2021]
-    imdb_2020 = df.loc[df['year_adjusted'] == 2020]
-    imdb_2019 = df.loc[df['year_adjusted'] == 2019]
-    imdb_2018 = df.loc[df['year_adjusted'] == 2018]
-    imdb_2017 = df.loc[df['year_adjusted'] == 2017]
-    imdb_2016 = df.loc[df['year_adjusted'] == 2016]
-    imdb_2015 = df.loc[df['year_adjusted'] == 2015]
-    imdb_2014 = df.loc[df['year_adjusted'] == 2014]
-    imdb_2013 = df.loc[df['year_adjusted'] == 2013]
-    imdb_2012 = df.loc[df['year_adjusted'] == 2012]
-    imdb_2011 = df.loc[df['year_adjusted'] == 2011]
-    imdb_2010 = df.loc[df['year_adjusted'] == 2010]
-    imdb_2009 = df.loc[df['year_adjusted'] == 2009]
-    imdb_2008 = df.loc[df['year_adjusted'] == 2008]
-    imdb_2007 = df.loc[df['year_adjusted'] == 2007]
-    imdb_2006 = df.loc[df['year_adjusted'] == 2006]
-    imdb_2005 = df.loc[df['year_adjusted'] == 2005]
-    imdb_2004 = df.loc[df['year_adjusted'] == 2004]
+    df_plot_dictionary = generate_imdb_dictionary_split_by_year(df=df, range_start=range_start,
+                                                                range_end=range_end,
+                                                                range_interval=range_interval)
 
-    sns.distplot(imdb_2004[['rating']], hist=False, rug=True, label='2004')
-    sns.distplot(imdb_2005[['rating']], hist=False, rug=True, label='2005')
-    sns.distplot(imdb_2006[['rating']], hist=False, rug=True, label='2006')
-    sns.distplot(imdb_2007[['rating']], hist=False, rug=True, label='2007')
-    sns.distplot(imdb_2008[['rating']], hist=False, rug=True, label='2008')
-    sns.distplot(imdb_2009[['rating']], hist=False, rug=True, label='2009')
-    sns.distplot(imdb_2010[['rating']], hist=False, rug=True, label='2010')
-    sns.distplot(imdb_2011[['rating']], hist=False, rug=True, label='2011')
-    sns.distplot(imdb_2012[['rating']], hist=False, rug=True, label='2012')
-    sns.distplot(imdb_2013[['rating']], hist=False, rug=True, label='2013')
-    sns.distplot(imdb_2014[['rating']], hist=False, rug=True, label='2014')
-    sns.distplot(imdb_2015[['rating']], hist=False, rug=True, label='2015')
-    sns.distplot(imdb_2016[['rating']], hist=False, rug=True, label='2016')
-    sns.distplot(imdb_2017[['rating']], hist=False, rug=True, label='2017')
-    sns.distplot(imdb_2018[['rating']], hist=False, rug=True, label='2018')
-    sns.distplot(imdb_2019[['rating']], hist=False, rug=True, label='2019')
-    sns.distplot(imdb_2020[['rating']], hist=False, rug=True, label='2020')
-    sns.distplot(imdb_2021[['rating']], hist=False, rug=True, label='2021')
-    sns.distplot(imdb_2022[['rating']], hist=False, rug=True, label='2022')
+    # we now plot these 20 individual lines onto the same graph
+    for year in df_plot_dictionary:
+        sns.distplot(df_plot_dictionary[year][['rating']], hist=False, rug=True, label=year)
+
     plt.legend()
     plt.xlabel("IMDb rating")
     plt.show()
 
-    # You can compare distribution of the two columns using two-sample Kolmogorov-Smirnov test, it is included in the scipy.stats:
-    # Here we note that none of the p values are less than 0.05 - not statistically significant at 5%
-    ks_2samp_2015_2016 = ks_2samp(imdb_2015['rating'], imdb_2016['rating'])
-    ks_2samp_2015_2021 = ks_2samp(imdb_2015['rating'], imdb_2021['rating'])
-    ks_2samp_2012_2016 = ks_2samp(imdb_2012['rating'], imdb_2016['rating'])
-    ks_2samp_2015_2022 = ks_2samp(imdb_2015['rating'], imdb_2022['rating'])
+    return df_plot_dictionary
+
+
+def generate_imdb_dictionary_split_by_year(df, range_start, range_end, range_interval):
+    """
+    Function which processes a dataframe containing imdb ratings, splits these up by year,
+    then returns a dictionary with the key:value pairs being:
+        key: year
+        value: dataframe filtered on the 'year_adjusted' equalling that year
+
+    :param df: dataframe containing imdb ratings for movies.
+    :param range_start: start of year range (default is 2004)
+    :param range_end: end of year range (default is 2023)
+    :param range_interval: interval of year range (default is 1)
+
+    :return: A dictionary containing:
+        key: year
+        value: dataframe filtered on the 'year_adjusted' equalling that year
+    """
+    years = [year for year in range(range_start, range_end, range_interval)]
+    return {y: df[df['year_adjusted'] == y] for y in years}
+
+
+def two_sample_kolmogorov_smirnov_test(df, year_one, year_two):
+    """
+    Function is to compare distribution of the two columns using two-sample Kolmogorov-Smirnov test, it is included in the scipy.stats:
+
+    Here we look for whether the p values are less than 0.05 - if they are, it means they are statistically significant at 5%
+
+    :param df: input imdb dataframe
+    :param year_one: the first year of the two we want to compare
+    :param year_two: the second year of the two we want to compare
+    :return:
+    """
+    year_one_df = df[year_one]
+    year_two_df = df[year_two]
+    kolmogorov_smirnov_value = ks_2samp(year_one_df['rating'], year_two_df['rating'])
+    return kolmogorov_smirnov_value
 
 
 if __name__ == '__main__':
-    df = get_average_imdb_ratings_all_movies()
-    plot_imdb_distributions(df)
+    df = plot_average_imdb_ratings_per_year(gross_filter=1000000, year_adjusted_filter=1999)
+    df = plot_imdb_distributions(df)
+
+    ks_2samp_2015_2016 = two_sample_kolmogorov_smirnov_test(df, '2015', '2016')
+    # ks_2samp_2015_2021 = two_sample_kolmogorov_smirnov_test(df, '2015', '2021')
+    # ks_2samp_2012_2016 = two_sample_kolmogorov_smirnov_test(df, '2012', '2016')
+    # ks_2samp_2015_2022 = two_sample_kolmogorov_smirnov_test(df, '2015', '2022')
